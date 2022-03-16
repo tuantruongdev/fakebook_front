fb4cdc26f680a8721426544904bd35c5b77b15e1
tuantruong <tuantruong.a8k29@gmail.com>
2022-03-16 15:23:39 +0700

    fix story

--- src/home/pages/newsfeed/Story.js
+++ src/home/pages/newsfeed/Story.js
@@ -1,9 +1,13 @@
 import React from "react";
 import style from "./Story.module.css";
+import avt from "../../../imgs/avt.jpg";
 export default function Story(props) {
   return (
     <div className={style["card-container"]}>
+      <img className={style["avt-container--unseen"]} alt="avt" src={avt} />
+
       <img className={style["img-container"]} alt="cac" src={props.url} />
+      <div className={style["owner-name"]}>Nguyễn Tuấn Trường</div>
     </div>
   );
 }
